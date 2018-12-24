aa870ffdf9554a871de3cd6fd9acd11f0621ddcc
credit23 <45500515+credit23@users.noreply.github.com>
2018-12-24 11:11:35 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("526701519324577832")
+setInterval(function() {
+channel.send(`هنا كلام ال سبام`);
+}, 30)
+})
+
+client.login("NTE4NDg3MzAxOTk5OTUxODcz.DwJA6w._BJATa7XeZDwYnIQhmhehH8PT3A");
